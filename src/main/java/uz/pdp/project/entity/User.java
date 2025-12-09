d41d792c1ec61dbd6b655cb0b783259b108e8bf5
Takhir <guvalakat1603@gmail.com>
2025-12-09 17:38:11 +0500

    User

--- src/main/java/uz/pdp/project/entity/User.java
+++ src/main/java/uz/pdp/project/entity/User.java
@@ -1,18 +1,41 @@
 package uz.pdp.project.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import uz.pdp.project.enums.UserStatus;
 
-@Data
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class User {
-    private Integer id;
-    private String firstName;
-    private String lastName;
+@Entity
+@ToString
+@Table(name = "users")
+public class User extends Auditable{
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status = UserStatus.IN_ACTIVE;
+
+    @Builder(builderMethodName = "childBuilder")
+    public User(String id, LocalDateTime createdAt, LocalDateTime updatedAt, String email, String password, String role, UserStatus status) {
+        super(id, createdAt, updatedAt);
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
 }
+// salom
+// dfasdfsfadf
+// salom
