13ced93a167183f6ab7811849e97d2b6bd33d398
Takhir <guvalakat1603@gmail.com>
2025-12-09 17:37:27 +0500

    Auditable

---
+++ src/main/java/uz/pdp/project/entity/Auditable.java
@@ -0,0 +1,27 @@
+package uz.pdp.project.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public abstract class Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition = "timestamp default now()", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
