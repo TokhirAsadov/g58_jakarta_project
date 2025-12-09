3fa4f9b200698032d043df835223b06c15d8fa83
Takhir <guvalakat1603@gmail.com>
2025-12-09 17:39:37 +0500

    MainServlet

---
+++ src/main/java/uz/pdp/project/servlet/MainServlet.java
@@ -0,0 +1,20 @@
+package uz.pdp.project.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "MainServlet", value = "")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MainServlet Servlet is working.......");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/main.jsp");
+        dispatcher.forward(req,resp);
+    }
+}
