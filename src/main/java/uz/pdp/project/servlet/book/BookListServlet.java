9946978b80d250d9c0b99f3a94166a59fabfd32e
Takhir <guvalakat1603@gmail.com>
2025-12-11 17:50:45 +0500

    BookListServlet

---
+++ src/main/java/uz/pdp/project/servlet/book/BookListServlet.java
@@ -0,0 +1,22 @@
+package uz.pdp.project.servlet.book;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "BookListServlet", value = "/book/list")
+public class BookListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Book List Servlet...");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/book/book_list.jsp");
+        dispatcher.forward(req,resp);
+    }
+
+}
