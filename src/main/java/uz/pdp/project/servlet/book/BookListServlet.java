d2c1878a1a0330b490bb0d0d7fd74010326ef0a4
Takhir <guvalakat1603@gmail.com>
2025-12-13 16:24:38 +0500

    BookListServlet

--- src/main/java/uz/pdp/project/servlet/book/BookListServlet.java
+++ src/main/java/uz/pdp/project/servlet/book/BookListServlet.java
@@ -6,15 +6,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import uz.pdp.project.dao.BookDAO;
+import uz.pdp.project.entity.Book;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "BookListServlet", value = "/book/list")
 public class BookListServlet extends HttpServlet {
+    private static final BookDAO bookDAO = new BookDAO();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Book List Servlet...");
+        List<Book> books = bookDAO.findAll();
+        req.setAttribute("books", books);
         RequestDispatcher dispatcher = req.getRequestDispatcher("/views/book/book_list.jsp");
         dispatcher.forward(req,resp);
     }
