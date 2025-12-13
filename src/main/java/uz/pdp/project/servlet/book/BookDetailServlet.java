b9c571ff953c0cffde1a1f4af8667fc98a348579
Takhir <guvalakat1603@gmail.com>
2025-12-13 17:21:12 +0500

    BookDetailServlet

--- src/main/java/uz/pdp/project/servlet/book/BookDetailServlet.java
+++ src/main/java/uz/pdp/project/servlet/book/BookDetailServlet.java
@@ -6,16 +6,24 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import uz.pdp.project.dao.BookDAO;
+import uz.pdp.project.entity.Book;
 
 import java.io.IOException;
 
 @WebServlet(name = "BookDetailServlet", urlPatterns = "/book/detail/*")
 public class BookDetailServlet extends HttpServlet {
 
+    private static final BookDAO bookDAO = new BookDAO();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Book List Servlet...");
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/book/book_list.jsp");
+        System.out.println("BookDetailServlet Servlet...");
+        String pathInfo = req.getPathInfo();
+        String bookId = pathInfo.substring(1);
+        Book book = bookDAO.findById(bookId);
+        req.setAttribute("book", book);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/book/book_detail.jsp");
         dispatcher.forward(req,resp);
     }
 
